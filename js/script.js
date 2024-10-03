f33f395959282e8ea9817b6b99bd7e1d99c779cb
vanessa <108832601+amanda29102007@users.noreply.github.com>
2024-10-03 08:38:36 -0300

    456789

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ function jogaNovamente() {
 
 function substituiNome() {
     for (const pergunta of perguntas) {
-        pergunta.enunciado = pergunta.enunciado.replace(você/g, nome);
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
     }
 }
 
